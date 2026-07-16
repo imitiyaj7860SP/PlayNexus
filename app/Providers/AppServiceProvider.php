c8ad00e7a64966fc1fbea7b66c49301db64fd8fb
imitiyaj7860SP <alikashav241@gmail.com>
2026-07-16 20:50:06 +0530

    Force HTTPS scheme in production to fix mixed content errors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,8 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        //
+{
+    if ($this->app->environment('production')) {
+        \Illuminate\Support\Facades\URL::forceScheme('https');
     }
 }
